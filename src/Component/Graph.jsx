b5b1d7a06a2a282603969413a3f4b506424c25b8
Fahad123k <ahmad.nishu08@gmail.com>
2024-11-13 15:54:26 +0530

    add fully responsive

--- src/Component/Graph.jsx
+++ src/Component/Graph.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   LineChart,
   Line,
@@ -6,7 +6,7 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
-  Legend
+  Legend,
 } from "recharts";
 
 const data = [
@@ -16,23 +16,52 @@ const data = [
   { name: "Page D", uv: 2780, pv: 3908, amt: 2000 },
   { name: "Page E", uv: 1890, pv: 4800, amt: 2181 },
   { name: "Page F", uv: 2390, pv: 3800, amt: 2500 },
-  { name: "Page G", uv: 3490, pv: 9800, amt: 2900 }
+  { name: "Page G", uv: 3490, pv: 9800, amt: 2900 },
 ];
 
 export default function Graph() {
+
+  const [chartWidth, setChartWidth] = useState(100); // Initial height
+
+  useEffect(() => {
+    // Function to update chart height based on window width
+    const updateChartWidth = () => {
+      if (window.innerWidth < 640) {
+        setChartWidth(100); // Small screens
+      } else if (window.innerWidth < 768) {
+        setChartWidth(150); // Medium screens
+      } else {
+        setChartWidth(185); // Large screens
+      }
+    };
+
+    // Set initial height and add event listener
+    updateChartWidth();
+    window.addEventListener('resize', updateChartWidth);
+
+    // Cleanup listener on component unmount
+    return () => window.removeEventListener('resize', updateChartWidth);
+  }, []);
+
   return (
     <LineChart
-      width={260}
-      height={100}
+      width={chartWidth}
+      height={chartWidth/2}
+      className="line-chart"
       data={data}
-      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+      // margin={{ top: 2, right: 10, left: 20, bottom: 5 }}
     >
       {/* <CartesianGrid strokeDasharray="3 3" /> */}
       {/* <XAxis dataKey="name" /> */}
       {/* <YAxis /> */}
       <Tooltip />
       <Legend />
-      <Line type="monotone" dataKey="pv" stroke="#4e545c" activeDot={{ r: 8 }} />
+      <Line
+        type="monotone"
+        dataKey="pv"
+        stroke="#4e545c"
+        activeDot={{ r: 8 }}
+      />
       <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
     </LineChart>
   );
